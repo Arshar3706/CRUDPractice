a0073bec757e110c81e32e82c1c06aae7f695cfb
Arshar3706 <epi3706@khu.ac.kr>
2020-12-06 16:44:15 +0900

    initial

---
+++ src/main/java/com/koesob/board/domain/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.koesob.board.domain.repository;
+
+import com.koesob.board.domain.entity.BoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+}
